6b45672cbef3cc3bc914f6658f4f0cd5eeadba29
Hady <sane_hady@hotmail.fr>
2021-09-20 09:31:55 +0200

    create files controllers

---
+++ backend/src/Controller/AlbumController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AlbumController extends AbstractController
+{
+    /**
+     * @Route("/album", name="album")
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/AlbumController.php',
+        ]);
+    }
+}
